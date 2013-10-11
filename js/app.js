855a38e3faaf021403c4213320925484487e7e18
Ajinkya <ajinkya@brandmovers.in>
2013-10-11 11:12:05 +0530

    Push me

---
+++ js/app.js
@@ -0,0 +1,20 @@
+var todoApp = angular.module('todoApp', ['ngResource']);
+
+todoApp.controller('AppCtrl', function($scope) {
+
+        //model
+        var items = [
+            {text: 'Start TODO App', done: true},
+            {text: 'Build routes', done: false}
+        ];
+
+        //add it to scope
+        $scope.items = items;
+
+        $scope.remaining = function() {
+          return items.reduce(function(count, item) { //reduce is ECMA5 method on an array
+              return item.done ? count : count + 1;
+          }, 0);
+        };
+    }
+);
